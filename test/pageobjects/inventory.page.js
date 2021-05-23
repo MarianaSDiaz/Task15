b8c6301ce9d37b1a1941de31f9063bc8673baa86
MarianaSDiaz <maruuus.95@gmail.com>
2021-05-23 18:51:33 -0300

    BQA-15: inventory tests added

--- test/pageobjects/inventory.page.js
+++ test/pageobjects/inventory.page.js
@@ -3,11 +3,15 @@ const LoginPage = require('../pageobjects/login.page');
 class InventoryPage {
     get dogImg () {return $$('[src="/static/media/sl-404.168b1cce.jpg"]')[0]}
     get realImg () {return $('[src="/static/media/sauce-backpack-1200x1500.34e7aa42.jpg"]')}
-    get menu () {return $('#react-burger-menu-btn')}
+    get menuBtn () {return $('#react-burger-menu-btn')}
     get allItemsBtn () {return $('#inventory_sidebar_link')}
     get aboutBtn () {return $('#about_sidebar_link')}
     get logoutBtn () {return $('#logout_sidebar_link')}
     get resetBtn () {return $('#reset_sidebar_link')}
+    get CompletMenu () {return $('#menu_button_container')}
+    get shopCart () {return $('#shopping_cart_container')}
+    get itemAdded () {return $$('.shopping_cart_badge')[0]}
+    get backPackAdd () {return $('#add-to-cart-sauce-labs-backpack')}
 
     open () {
         LoginPage.open();
--- test/specs/inventory.js
+++ test/specs/inventory.js
@@ -1,3 +1,4 @@
+const inventoryPage = require("../pageobjects/inventory.page");
 const InventoryPage = require("../pageobjects/inventory.page");
 const LoginPage = require("../pageobjects/login.page");
 
@@ -10,11 +11,37 @@ describe("Inventory automation", () =>{
     
     describe("Testing the burguer menu", () => {
         it("testing the menu buttons exist", () => {
-            expect(InventoryPage.menu).toExist();
+            expect(InventoryPage.menuBtn).toExist();
             expect(InventoryPage.allItemsBtn).toExist();
             expect(InventoryPage.aboutBtn).toExist();
             expect(InventoryPage.logoutBtn).toExist();
             expect(InventoryPage.resetBtn).toExist();
-        })
-    })
+        });
+        it("testing about button function", () => {
+            InventoryPage.menuBtn.click();
+            InventoryPage.aboutBtn.click();
+            expect(browser).toHaveUrl("https://saucelabs.com/")
+        });
+        it("testing logout button function", () => {
+            InventoryPage.open()
+            InventoryPage.menuBtn.click();
+            InventoryPage.logoutBtn.click();
+            expect(browser).toHaveUrl("https://www.saucedemo.com/")
+        });
+        it("testing reset button function", () => {
+            InventoryPage.open()
+            InventoryPage.backPackAdd.click();
+            InventoryPage.menuBtn.click();
+            InventoryPage.resetBtn.click();
+            expect(InventoryPage.itemAdded).not.toExist()
+        });
+    });
+    describe("Testing add to cart button", () => {
+        it("the cart should have the item added", () => {
+            InventoryPage.open();
+            InventoryPage.backPackAdd.click();
+            expect(InventoryPage.itemAdded).toHaveText("1");
+            browser.pause(3000);
+        });
+    });
 });
